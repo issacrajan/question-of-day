4103958e112e0ce85ba2bda9ae1753a786767bdc
issac <issac.rajan@gmail.com>
2023-12-31 14:41:46 +0530

    changes

--- src/day1/Day01.java
+++ src/day1/Day01.java
@@ -25,7 +25,7 @@ public class Day01 {
 	 * 
 	 * KEY -- Value 
 	 * JAN -- 2023-01-21 
-	 * FEB -- 2023-01-25 
+	 * FEB -- 2023-02-25 
 	 * MAR -- 2023-03-29
 	 * 
 	 * 
@@ -33,6 +33,29 @@ public class Day01 {
 	//@formatter:on
 	private void process() {
 		// write your code here
+
+		for (String input : inputList) {
+			int index = input.indexOf('=');// separator for month and day
+			String month = input.substring(0, index);
+			String day = input.substring(index + 1, input.length());
+
+			if (monthWiseMaxDayMap.containsKey(month)) {
+				String maxDay = monthWiseMaxDayMap.get(month);
+				int inpuDay = Integer.parseInt(day.substring(day.length() - 2, day.length()));
+				int existingDay = Integer
+						.parseInt(maxDay.substring(maxDay.length() - 2, maxDay.length()));
+				if (inpuDay > existingDay) {
+
+					monthWiseMaxDayMap.put(month, day);
+				}
+			} else {
+				monthWiseMaxDayMap.put(month, day);
+			}
+
+		}
+		for (String key : monthWiseMaxDayMap.keySet()) {
+			System.out.println("KEY:" + key + " " + "VALUE:" + monthWiseMaxDayMap.get(key));
+		}
 	}
 
 	private void initData() {
@@ -40,8 +63,8 @@ public class Day01 {
 		inputList.add("JAN=2023-01-11");
 		inputList.add("JAN=2023-01-21");
 
-		inputList.add("FEB=2023-01-25");
-		inputList.add("FEB=2023-01-01");
+		inputList.add("FEB=2023-02-25");
+		inputList.add("FEB=2023-02-01");
 
 		inputList.add("FEB=2023-03-01");
 		inputList.add("MAR=2023-03-29");
